31dcb5ff4525fb09f3501e4ae234860265c23cb8
Fabio Muller <fabio.muller@fael.edu.br>
2020-05-13 17:31:24 -0300

    integração chimpmail ajustada 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,156 +1,157 @@
-function quizz() {
+// function quizz() {
 
-  const questions = [
-    {
-      question: "Qual seria sua foto de perfil ideal?",
-      alternatives: [
-        {
-          text: "Uma foto linda, com a luz ideial, paisagem bonita e que mostrasse o meu melhor lado.",
-          img: "https://images.unsplash.com/photo-1583001809760-b94c68937141?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 1
-        },
-        {
-          text: "Uma foto poética que diz um pouco sobre o que eu penso e sou e não só sobre minha aparência.",
-          img: "https://images.unsplash.com/photo-1474366521946-c3d4b507abf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 2
+//   const questions = [
+//     {
+//       question: "Qual seria sua foto de perfil ideal?",
+//       alternatives: [
+//         {
+//           text: "Uma foto linda, com a luz ideial, paisagem bonita e que mostrasse o meu melhor lado.",
+//           img: "https://images.unsplash.com/photo-1583001809760-b94c68937141?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 1
+//         },
+//         {
+//           text: "Uma foto poética que diz um pouco sobre o que eu penso e sou e não só sobre minha aparência.",
+//           img: "https://images.unsplash.com/photo-1474366521946-c3d4b507abf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 2
 
-        },
-        {
-          text: "Um meme com uma sacada ideal e que gerasse algumas risadas.",
-          img: "https://images.unsplash.com/photo-1505682614136-0a12f9f7beea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 3
+//         },
+//         {
+//           text: "Um meme com uma sacada ideal e que gerasse algumas risadas.",
+//           img: "https://images.unsplash.com/photo-1505682614136-0a12f9f7beea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 3
 
-        },
-        {
-          text: "Uma foto de perfil ideal seria aquela que me deixasse bonita, mas que capturasse as minhas idéias também.",
-          img: "https://images.unsplash.com/photo-1530497610245-94d3c16cda28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 4
+//         },
+//         {
+//           text: "Uma foto de perfil ideal seria aquela que me deixasse bonita, mas que capturasse as minhas idéias também.",
+//           img: "https://images.unsplash.com/photo-1530497610245-94d3c16cda28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 4
 
-        }
-      ]
-    },
+//         }
+//       ]
+//     },
 
-    {
-      question: "Como você postaria seu momento com o seu crush?",
-      alternatives: [
-        {
-          text: "Nosso amor é lindo e todos presicam ver isso! Precisamos aparecer bem agarrados e demonstrando nosso amor! O cenário tem que ser romântico claro!",
-          img: "https://images.unsplash.com/photo-1522659536630-ce1b6286426f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 1
-        },
-        {
-          text: "Nada de fotos agarradinho ou beijinho. Talvez algo único que vivemos, algo que tenha uma história",
-          img: "https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 2
+//     {
+//       question: "Como você postaria seu momento com o seu crush?",
+//       alternatives: [
+//         {
+//           text: "Nosso amor é lindo e todos presicam ver isso! Precisamos aparecer bem agarrados e demonstrando nosso amor! O cenário tem que ser romântico claro!",
+//           img: "https://images.unsplash.com/photo-1522659536630-ce1b6286426f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 1
+//         },
+//         {
+//           text: "Nada de fotos agarradinho ou beijinho. Talvez algo único que vivemos, algo que tenha uma história",
+//           img: "https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 2
 
-        },
-        {
-          text: "Postaria alguma trollagem ou foto que não seja perfeita o com o cenário ideal, mas que mostre o quanto no amamos e nos divertimos",
-          img: "https://images.unsplash.com/photo-1525107989215-a4921d19cc90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 3
+//         },
+//         {
+//           text: "Postaria alguma trollagem ou foto que não seja perfeita o com o cenário ideal, mas que mostre o quanto no amamos e nos divertimos",
+//           img: "https://images.unsplash.com/photo-1525107989215-a4921d19cc90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 3
 
-        },
-        {
-          text: "Postaria o momento que estamos juntos, o cenário.Niguém precisa de detalhes do que acontece",
-          img: "https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 4
+//         },
+//         {
+//           text: "Postaria o momento que estamos juntos, o cenário.Niguém precisa de detalhes do que acontece",
+//           img: "https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 4
 
-        }
-      ]
-    },
-    {
-      question: "Quando você vê uma foto das suas amigas, você:",
-      alternatives: [
-        {
-          text: "Dá aquela curtida e comenta para ver a reação. Amiga é pra isso, né?",
-          img: "https://images.unsplash.com/photo-1526929804231-3de5919d43ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 1
-        },
-        {
-          text: "Curto apenas aquelas que saiam do padrão. Quem quer ver sua comida?",
-          img: "https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 2
+//         }
+//       ]
+//     },
+//     {
+//       question: "Quando você vê uma foto das suas amigas, você:",
+//       alternatives: [
+//         {
+//           text: "Dá aquela curtida e comenta para ver a reação. Amiga é pra isso, né?",
+//           img: "https://images.unsplash.com/photo-1526929804231-3de5919d43ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 1
+//         },
+//         {
+//           text: "Curto apenas aquelas que saiam do padrão. Quem quer ver sua comida?",
+//           img: "https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 2
 
-        },
-        {
-          text: "Curto aquelas em que eu vejo a alegria da galera reunida. POR QUE NÃO FUI CHAMADA, HEIN?",
-          img: "https://images.unsplash.com/photo-1519671482749-fd09be7ccebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 3
+//         },
+//         {
+//           text: "Curto aquelas em que eu vejo a alegria da galera reunida. POR QUE NÃO FUI CHAMADA, HEIN?",
+//           img: "https://images.unsplash.com/photo-1519671482749-fd09be7ccebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 3
 
-        },
-        {
-          text: "Nem dou atenção. Sério mesmo que você vai postar isso aí?",
-          img: "https://images.unsplash.com/photo-1516222338250-863216ce01ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 4
+//         },
+//         {
+//           text: "Nem dou atenção. Sério mesmo que você vai postar isso aí?",
+//           img: "https://images.unsplash.com/photo-1516222338250-863216ce01ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 4
 
-        }
-      ]
-    },
+//         }
+//       ]
+//     },
 
-    {
-      question: "Qual foto com o seu pet você postaria?",
-      alternatives: [
-        {
-          text: "Claro que seria o momento mais fofo do mundo! Eu abraçada (o) com o meu pet, e de preferência que os dois estivessem olhando pra câmera!",
-          img: "https://images.unsplash.com/photo-1526579903323-327e1c2d0b27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 1
-        },
-        {
-          text: "Gosto de postar fotos diferentes. As redes sociais estão cheias de fotos de pessoas apenas segurando o pet e mais nada.",
-          img: "https://images.unsplash.com/photo-1501820488136-72669149e0d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 2
+//     {
+//       question: "Qual foto com o seu pet você postaria?",
+//       alternatives: [
+//         {
+//           text: "Claro que seria o momento mais fofo do mundo! Eu abraçada (o) com o meu pet, e de preferência que os dois estivessem olhando pra câmera!",
+//           img: "https://images.unsplash.com/photo-1526579903323-327e1c2d0b27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 1
+//         },
+//         {
+//           text: "Gosto de postar fotos diferentes. As redes sociais estão cheias de fotos de pessoas apenas segurando o pet e mais nada.",
+//           img: "https://images.unsplash.com/photo-1501820488136-72669149e0d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 2
 
-        },
-        {
-          text: "Com certeza seria algum vídeo do meu pet fazendo algo super engraçado!",
-          img: "https://images.unsplash.com/photo-1573685363359-e3b180520518?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 3
+//         },
+//         {
+//           text: "Com certeza seria algum vídeo do meu pet fazendo algo super engraçado!",
+//           img: "https://images.unsplash.com/photo-1573685363359-e3b180520518?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 3
 
-        },
-        {
-          text: "Postaria uma foto de algum momento especial nosso. Se o meu pet está feliz na foto, já está ótimo pra mim!",
-          img: "https://images.unsplash.com/photo-1567459092904-0fd49a7265e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
-          answer: 4
+//         },
+//         {
+//           text: "Postaria uma foto de algum momento especial nosso. Se o meu pet está feliz na foto, já está ótimo pra mim!",
+//           img: "https://images.unsplash.com/photo-1567459092904-0fd49a7265e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+//           answer: 4
 
-        }
-      ]
-    },
-  ]
+//         }
+//       ]
+//     },
+//   ]
 
-  const answers = [
-    {
-      id: 1,
-      title: "Sou linda e quero que todo mundo saiba disso!",
-      text: "Você ama compartilhar os eventos da sua vida. Procurar sempre a melhor luz e o melhor ângulo pra tirar a foto perfeita e que, por favor, essa foto gere muuuuuuuuitos likes!",
-      img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/Y55wGmP3W.gif?output-format=mp4&output-quality=auto"
-    },
-    {
-      id: 2,
-      title: "Tenho estilo próprio e não faço o que todo mundo faz",
-      text: "Eu não gosto de postar foto e seguir modinha não é comigo. Posto o que é diferente e inovador",
-      img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/-NA7tL-yf.gif?output-format=mp4&output-quality=auto"
-    },
-    {
-      id: 4,
-      title: "Posto o que é importante e relevante para mim e meus amigos",
-      text: "Gosto de ver os que as pessoas postam e se me der vontade eu posto o que quero no momento. Ou não.",
-      img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/WL-hMmGJj.gif?output-format=mp4&output-quality=auto"
-    },
-    {
-      id: 3,
-      title: "Me divirto e, se puder, me divirto mais ainda!",
-      text: "Você gosta de interagir com os amigos e compartilhar muitos memes... Por favor, gere muitas gargalhadas! Só não vai perder os óculos, hein?",
-      img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/O5XE1YUus.gif?output-format=mp4&output-quality=auto"
-    }
-  ]
-
-
-
-
+//   const answers = [
+//     {
+//       id: 1,
+//       title: "Sou linda e quero que todo mundo saiba disso!",
+//       text: "Você ama compartilhar os eventos da sua vida. Procurar sempre a melhor luz e o melhor ângulo pra tirar a foto perfeita e que, por favor, essa foto gere muuuuuuuuitos likes!",
+//       img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/Y55wGmP3W.gif?output-format=mp4&output-quality=auto"
+//     },
+//     {
+//       id: 2,
+//       title: "Tenho estilo próprio e não faço o que todo mundo faz",
+//       text: "Eu não gosto de postar foto e seguir modinha não é comigo. Posto o que é diferente e inovador",
+//       img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/-NA7tL-yf.gif?output-format=mp4&output-quality=auto"
+//     },
+//     {
+//       id: 4,
+//       title: "Posto o que é importante e relevante para mim e meus amigos",
+//       text: "Gosto de ver os que as pessoas postam e se me der vontade eu posto o que quero no momento. Ou não.",
+//       img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/WL-hMmGJj.gif?output-format=mp4&output-quality=auto"
+//     },
+//     {
+//       id: 3,
+//       title: "Me divirto e, se puder, me divirto mais ainda!",
+//       text: "Você gosta de interagir com os amigos e compartilhar muitos memes... Por favor, gere muitas gargalhadas! Só não vai perder os óculos, hein?",
+//       img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/O5XE1YUus.gif?output-format=mp4&output-quality=auto"
+//     }
+//   ]
 
   
+// }
+
+
+function sendmail() {
+  const name = document.getElementById("name").value
+  const email = document.getElementById("email").value
+  document.getElementById("mce-FNAME").value = name
+  document.getElementById("mce-EMAIL").value = email
+  // document.getElementById("mc-embedded-subscribe").click();
 }
-
-
-
-
